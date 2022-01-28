534df3cc4f961511758b6717ece5b78917eda5af
Mimi <63885221+Mimi2020hub@users.noreply.github.com>
2022-01-28 10:38:28 -0500

    Add files via upload

---
+++ Project3-ShortestPathAlg.py
@@ -0,0 +1,16 @@
+def startwith(start: int, mgraph: list) -> list:
+
+    return dis
+
+
+if __name__ == "__main__":
+    inf = 10086
+    mgraph = [[0, 1, 12, inf, inf, inf],
+              [inf, 0, 9, 3, inf, inf],
+              [inf, inf, 0, inf, 5, inf],
+              [inf, inf, 4, 0, 13, 15],
+              [inf, inf, inf, inf, 0, 4],
+              [inf, inf, inf, inf, inf, 0]]
+
+    dis = startwith(0, mgraph)
+    print(1)
